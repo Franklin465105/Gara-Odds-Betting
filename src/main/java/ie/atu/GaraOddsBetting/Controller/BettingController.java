bdf155d8e8ba5a546f2015a73223b64e349283cb
Franklin Esedebe <G00465105@atu.ie>
2026-04-29 23:16:49 +0100

    Exception Handler, Service file and controller set up. Ready to be linked to our previous microservice for completion.

--- src/main/java/ie/atu/GaraOddsBetting/Controller/BettingController.java
+++ src/main/java/ie/atu/GaraOddsBetting/Controller/BettingController.java
@@ -1,4 +1,52 @@
 package ie.atu.GaraOddsBetting.Controller;
 
+import ie.atu.GaraOddsBetting.Model.Bet;
+import ie.atu.GaraOddsBetting.Service.BettingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/bets")
 public class BettingController {
+
+    private final BettingService bettingService;
+
+    public BettingController(BettingService bettingService) {
+        this.bettingService = bettingService;
+    }
+
+    // places a bet for the user
+    @PostMapping("/place")
+    public ResponseEntity<?> placeBet(@RequestParam String username, @RequestParam double amount, @RequestParam double odds) {
+
+        // stops negative or 0 bet amount
+        if (amount <= 0) {
+            return ResponseEntity.badRequest().body("Please enter a positive amount");
+        }
+
+        // odds must be greater than 1 to make sense
+        if (odds <= 1.0) {
+            return ResponseEntity.badRequest().body("Odds must be greater than 1.0");
+        }
+
+        Bet bet = bettingService.placeBet(username, amount, odds);
+
+        // returns different message depending on if the user won or lost
+        if (bet.getStatus().equals("WON")) {
+            return ResponseEntity.ok("You WON! Potential winnings: €" + bet.getPotentialWinnings() + ". Bet ID: " + bet.getBetId());
+        }
+        return ResponseEntity.ok("You LOST. €" + amount + " was your stake. Bet ID: " + bet.getBetId());
+    }
+
+    // shows all bets placed by a user
+    @GetMapping
+    public ResponseEntity<?> getBets(@RequestParam String username) {
+
+        // checks if the user has any bets
+        if (!bettingService.hasBets(username)) {
+            return ResponseEntity.badRequest().body("No bets found for " + username);
+        }
+
+        return ResponseEntity.ok(bettingService.getBetsForUser(username));
+    }
 }
--- src/main/java/ie/atu/GaraOddsBetting/Service/BettingService.java
+++ src/main/java/ie/atu/GaraOddsBetting/Service/BettingService.java
@@ -1,4 +1,53 @@
 package ie.atu.GaraOddsBetting.Service;
 
+import ie.atu.GaraOddsBetting.Model.Bet;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
 public class BettingService {
+
+    private final List<Bet> bets = new ArrayList<>();
+    private final Random random = new Random();
+    private int nextId = 1;
+
+    // places a bet and randomly decides if the user wins or loses
+    public Bet placeBet(String username, double amount, double odds) {
+        boolean won = random.nextBoolean();
+
+        Bet bet = new Bet();
+        bet.setBetId(nextId++);
+        bet.setUsername(username);
+        bet.setAmount(amount);
+        bet.setOdds(odds);
+        bet.setPotentialWinnings(amount * odds);
+        bet.setStatus(won ? "WON" : "LOST");
+        bets.add(bet);
+
+        return bet;
+    }
+
+    // gets all bets placed by a user
+    public List<Bet> getBetsForUser(String username) {
+        List<Bet> userBets = new ArrayList<>();
+        for (Bet bet : bets) {
+            if (bet.getUsername().equals(username)) {
+                userBets.add(bet);
+            }
+        }
+        return userBets;
+    }
+
+    // checks if a user has any bets
+    public boolean hasBets(String username) {
+        for (Bet bet : bets) {
+            if (bet.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
